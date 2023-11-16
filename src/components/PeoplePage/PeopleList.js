26eeec95e4c58b27fc077d2cb1827bf7e965ccdb
Kamil Sajdera <kamilsajdera@gmail.com>
2023-11-16 17:23:53 +0100

    Added sort by popularity function and improved design

--- src/components/PeoplePage/PeopleList.js
+++ src/components/PeoplePage/PeopleList.js
@@ -1,11 +1,26 @@
 import classes from "./PeopleList.module.css";
 
 const PeopleList = ({ trendingPeople }) => {
+  const comparedList = trendingPeople.sort(
+    (a, b) => b.popularity - a.popularity
+  );
+
+  const itemClassname = (index) =>
+    `${classes["person-item"]} ${
+      index === 0
+        ? classes.first_place
+        : index === 1
+        ? classes.second_place
+        : index === 2
+        ? classes.third_place
+        : ""
+    }`;
 
   return (
     <section className={classes.popularPeopleWrapper}>
-      {trendingPeople.map((person) => (
-        <div className={classes["person-item"]} key={person.id}>
+      {comparedList.map((person, index) => (
+        <div className={itemClassname(index)} key={person.id}>
+          <div className={classes.number}>{index + 1}</div>
           <div className={classes.image_box}>
             <img
               src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
@@ -18,7 +33,7 @@ const PeopleList = ({ trendingPeople }) => {
             Popularity: {person.popularity}
           </div>
           <div className={classes.person_films}>
-            { person.known_for.map(item => item.title).join(", ")}
+            {person.known_for.map((item) => item.title).join(", ")}
           </div>
         </div>
       ))}
