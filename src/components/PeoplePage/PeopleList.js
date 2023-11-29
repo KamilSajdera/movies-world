787111b8339b3a1de1f7f234db29d41e34334a7d
Kamil Sajdera <kamilsajdera@gmail.com>
2023-11-29 11:41:03 +0100

    Added forwarding to a specific person profile

--- src/components/PeoplePage/PeopleList.js
+++ src/components/PeoplePage/PeopleList.js
@@ -1,6 +1,11 @@
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import classes from "./PeopleList.module.css";
 
 const PeopleList = ({ trendingPeople, currentPage }) => {
+  const peopleItemsRef = useRef();
+  const navigate = useNavigate();
+
   const comparedList = trendingPeople.sort(
     (a, b) => b.popularity - a.popularity
   );
@@ -16,11 +21,34 @@ const PeopleList = ({ trendingPeople, currentPage }) => {
         : ""
     }`;
 
+  useEffect(() => {
+    const getPersonProfile = (event) => {
+      const profileId = event.currentTarget.getAttribute("data-key");
+      navigate(`/people/profile/${profileId}`);
+    };
+
+    const peopleItems = peopleItemsRef.current.querySelectorAll(
+      `.${classes["person-item"]}`
+    );
+
+    peopleItems.forEach((item) => {
+      item.addEventListener("click", getPersonProfile);
+    });
+
+    return () => {
+      peopleItems.forEach((item) => {
+        item.removeEventListener("click", getPersonProfile);
+      });
+    };
+  }, [comparedList, navigate]);
+
   return (
-    <section className={classes.popularPeopleWrapper}>
+    <section className={classes.popularPeopleWrapper} ref={peopleItemsRef}>
       {comparedList.map((person, index) => (
-        <div className={itemClassname(index)} key={person.id}>
-          <div className={classes.number}>{20 * currentPage - 20 + (index + 1)}</div>
+        <div className={itemClassname(index)} key={person.id} data-key={person.id}>
+          <div className={classes.number}>
+            {20 * currentPage - 20 + (index + 1)}
+          </div>
           <div className={classes.image_box}>
             <img src={person.image} alt={`${person.name}'s img`} />
           </div>
