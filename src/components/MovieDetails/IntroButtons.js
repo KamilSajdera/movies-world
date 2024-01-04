886f7b0d02c9c185146890fe62d4e497fbb8361d
Kamil Sajdera <kamilsajdera@gmail.com>
2024-01-04 20:17:43 +0100

    Added smooth scroll to trailer container after click 'Watch trailer' button.

--- src/components/MovieDetails/IntroButtons.js
+++ src/components/MovieDetails/IntroButtons.js
@@ -1,17 +1,36 @@
 import classes from "./IntroButtons.module.css";
+import trailerClass from "./MovieTrailer.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faPlay } from "@fortawesome/free-solid-svg-icons";
 
+import MoveTo from "moveto";
+
 const IntroButtons = ({ homepage, trailer }) => {
+  
+  const moveToOptions = {
+    tolerance: 20,
+    duration: 1000,
+  };
+
+  const moveTo = new MoveTo(moveToOptions);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const target = document.querySelector(`.${trailerClass.trailerContainer}`);
+    moveTo.move(target);
+  };
+
   return (
     <>
-      <button className={classes.btnWatchTrailer}>
-        <b>
-          <FontAwesomeIcon icon={faPlay} />
-          Watch trailer
-        </b>
-      </button>
+      {trailer && (
+        <button className={classes.btnWatchTrailer} onClick={handleClick}>
+          <b>
+            <FontAwesomeIcon icon={faPlay} />
+            Watch trailer
+          </b>
+        </button>
+      )}
       {homepage && (
         <a href={homepage} target="_blank" rel="noreferrer">
           <button className={classes.btnVisitPage}>
