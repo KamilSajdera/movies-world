3caf6654c8048a73a753ebd0bf4e3f7b436333a8
Kamil Sajdera <kamilsajdera@gmail.com>
2024-01-05 19:03:51 +0100

    Added navigate to every actor's profile in cast section

--- src/components/MovieDetails/DisplayMovieActors.js
+++ src/components/MovieDetails/DisplayMovieActors.js
@@ -1,6 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import classes from "./DisplayMovieActors.module.css";
 
 const DisplayMovieActors = ({ cast }) => {
+
+  const navigate = useNavigate();
+
+  const navigateToProfile = (id, name) => {
+    const urlName = name.toLowerCase().replace(/\s+/g, '-').trim();
+    navigate(`../people/profile/${id}-${urlName}`)
+  };
+
   return (
     <div className={classes.castContent}>
       <h1>Cast</h1>
@@ -8,7 +17,7 @@ const DisplayMovieActors = ({ cast }) => {
         {cast.map((item) => {
           const actorProfileImg = `https://image.tmdb.org/t/p/w500${item.profile_path}`;
           return (
-            <div key={item.id} className={classes["actor-item"]}>
+            <div key={item.id} className={classes["actor-item"]} onClick={() => navigateToProfile(item.id, item.name)}>
               <img src={actorProfileImg} alt="Profile img" />
               <h3>{item.name}</h3>
               <p>
