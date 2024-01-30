c1b50727cce712b747ef00bbbb013c49eb88fad5
Kamil Sajdera <kamilsajdera@gmail.com>
2024-01-30 17:44:53 +0100

    Added message about lack of informations about similar/recommentadion movies and reviews

--- src/components/MovieDetails/ReviewsSection.js
+++ src/components/MovieDetails/ReviewsSection.js
@@ -1,19 +1,24 @@
 import classes from "./ReviewsSection.module.css";
 
 const ReviewsSection = ({ reviews }) => {
+  let content =
+    reviews.length > 0
+      ? reviews.map((item, index) => {
+          const date = item.created_at.slice(0, 10);
+          return (
+            <div className={classes["review-item"]} key={index}>
+              <div className={classes["author_name"]}>{item.author}</div>
+              <div className={classes["review_date"]}>{date}</div>
+              <div className={classes["review_content"]}>{item.content}</div>
+            </div>
+          );
+        })
+      : <p className={classes.noReviewsMsg}>This movie doesn't have any reviews in our database yet.</p>
+
   return (
     <section className={classes.reviewsContainer}>
       <h1>Reviews</h1>
-      {reviews.map((item, index) => {
-        const date = item.created_at.slice(0,10)
-        return (
-          <div className={classes["review-item"]} key={index}>
-            <div className={classes["author_name"]}>{item.author}</div>
-            <div className={classes["review_date"]}>{date}</div>
-            <div className={classes["review_content"]}>{item.content}</div>
-          </div>
-        );
-      })}
+      {content}
     </section>
   );
 };
--- src/components/MovieDetails/SimilarFilms.js
+++ src/components/MovieDetails/SimilarFilms.js
@@ -5,15 +5,13 @@ const SimilarFilms = ({ movies, title }) => {
   const navigate = useNavigate();
 
   const navigateToMovie = (id, name) => {
-    const urlName = name.toLowerCase().replace(/\s+/g, '-').trim();
-    navigate(`/movie/${id}-${urlName}`)
+    const urlName = name.toLowerCase().replace(/\s+/g, "-").trim();
+    navigate(`/movie?id=${id}-${urlName}`);
   };
 
-  return (
-    <div className={classes.similarWrapper}>
-      <h1>{title}</h1>
-      <div className={classes.moviesItems}>
-        {movies.map((movie) => (
+  let content =
+    movies.length > 0
+      ? movies.map((movie) => (
           <div
             className={classes["movie-item"]}
             key={movie.id}
@@ -25,8 +23,13 @@ const SimilarFilms = ({ movies, title }) => {
             />
             <p>{movie.title}</p>
           </div>
-        ))}
-      </div>
+        ))
+      : <p className={classes.noInfoMessage}>We don't have informations about {title} for this movie.</p>
+
+  return (
+    <div className={classes.similarWrapper}>
+      <h1>{title}</h1>
+      <div className={classes.moviesItems}>{content}</div>
     </div>
   );
 };
