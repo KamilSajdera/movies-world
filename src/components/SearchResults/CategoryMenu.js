5b80a58f233866a6bc37bfebaf305270e6348efc
Kamil Sajdera <kamilsajdera@gmail.com>
2024-02-11 14:22:48 +0100

    Added support for changing categories and displaying appropriate feedback

--- src/components/SearchResults/CategoryMenu.js
+++ src/components/SearchResults/CategoryMenu.js
@@ -1,21 +1,38 @@
 import classes from "./CategoryMenu.module.css";
 
-const CategoryMenu = ({ results }) => {
+const CategoryMenu = (props) => {
+  const { results, categoryNr } = props;
+
   return (
     <div className={classes.results_category}>
-      <div className={classes["category-item"]}>
+      <div
+        className={`${classes["category-item"]} ${
+          categoryNr === 1 ? classes.category_active : ""
+        }`}
+        onClick={() => props.onChangeCategory(1)}
+      >
         Movies
         <span className={classes["category-amout"]}>
           {results.movies.total_results}
         </span>
       </div>
-      <div className={classes["category-item"]}>
+      <div
+        className={`${classes["category-item"]} ${
+          categoryNr === 2 ? classes.category_active : ""
+        }`}
+        onClick={() => props.onChangeCategory(2)}
+      >
         Series
         <span className={classes["category-amout"]}>
           {results.series.total_results}
         </span>
       </div>
-      <div className={classes["category-item"]}>
+      <div
+        className={`${classes["category-item"]} ${
+          categoryNr === 3 ? classes.category_active : ""
+        }`}
+        onClick={() => props.onChangeCategory(3)}
+      >
         People
         <span className={classes["category-amout"]}>
           {results.people.total_results}
--- src/components/SearchResults/ResultsContent.js
+++ src/components/SearchResults/ResultsContent.js
@@ -1,17 +1,26 @@
+
 import classes from "./ResultsContent.module.css";
 
 import CategoryMenu from "./CategoryMenu";
 import MoviesSection from "./MoviesSection";
+import { useState } from "react";
 
 const ResultsContent = ({ results, title }) => {
- 
+  const [categoryNr, setCategoryNr] = useState(1)
+
+  const changeCategoryHandle = (value) => 
+  {
+    if(categoryNr !== value)
+      setCategoryNr(value)
+  }
+
   return (
     <div className={classes.resultsContent}>
       <h3>
         Search results for: <b>{title}</b>
       </h3>
-      <CategoryMenu results={results} />
-      <MoviesSection movies={results.movies.results} />
+      <CategoryMenu results={results} categoryNr={categoryNr} onChangeCategory={changeCategoryHandle} />
+      { categoryNr === 1 && <MoviesSection movies={results.movies.results} /> }
     </div>
   );
 };
