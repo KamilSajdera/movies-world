46ea3999114f93069424919a8e070cc2f11744b3
Kamil Sajdera <kamilsajdera@gmail.com>
2024-02-16 18:35:19 +0100

    Added redirect to tv serial details page from series section in search term page

--- src/components/SearchResults/SeriesSection.js
+++ src/components/SearchResults/SeriesSection.js
@@ -1,23 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
 import classes from "./ResultsContent.module.css";
 import noImage from "../../assets/people/noImage.png";
 
 const SeriesSection = ({ series }) => {
+  const navigate = useNavigate();
 
   return (
     <div className={classes["results_items"]}>
       {series.map((serial) => {
-        const date = new Date(serial.first_air_date).toLocaleDateString("en-ES", {
-          day: "numeric",
-          month: "long",
-          year: "numeric",
-        });
+        const date = new Date(serial.first_air_date).toLocaleDateString(
+          "en-ES",
+          {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          }
+        );
 
         const image = serial.poster_path
           ? `https://image.tmdb.org/t/p/w500/${serial.poster_path}`
           : noImage;
 
+        const navigateUrl = `/tv?id=${serial.id}-${serial.name
+          .toLowerCase()
+          .replace(/\s+/g, "-")
+          .trim()}`;
+
         return (
-          <div className={classes["result-item"]} key={serial.id}>
+          <div
+            className={classes["result-item"]}
+            key={serial.id}
+            onClick={() => navigate(navigateUrl)}
+          >
             <div className={classes["result-img"]}>
               <img src={image} alt="alt" />
             </div>
@@ -29,7 +44,7 @@ const SeriesSection = ({ series }) => {
           </div>
         );
       })}
-    {series.length === 0 && <p>No data for section series.</p>}
+      {series.length === 0 && <p>No data for section series.</p>}
     </div>
   );
 };
