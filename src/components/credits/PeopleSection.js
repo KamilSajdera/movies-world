8276b14ce35770c4aea635bd6ce42f97dca708f3
Kamil Sajdera <kamilsajdera@gmail.com>
2024-03-03 14:05:28 +0100

    People are now displayed without repetitions if they have more than 1 record in 'department'. All crew member roles are in 1 array.

--- src/components/credits/PeopleSection.js
+++ src/components/credits/PeopleSection.js
@@ -4,17 +4,41 @@ import classes from "./PeopleSection.module.css";
 import noImage from "../../assets/people/noProfileImage.png";
 
 const PeopleSection = ({ people, title }) => {
+  
   const navigate = useNavigate();
+  let uniquePeople = [];
 
   const navigateToProfile = (id, name) => {
     const urlName = name.toLowerCase().replace(/\s+/g, "-").trim();
     navigate(`/people/profile?id=${id}-${urlName}`);
   };
 
+  const readyPeople = () => {
+    people.forEach((person) => {
+      const obj = {
+        id: person.id,
+        name: person.name,
+        profile_path: person.profile_path,
+        roles: person.roles,
+        department: [person.department || person.character],
+      };
+
+      const existingIndex = uniquePeople.findIndex((item) => item.id === person.id);
+
+      if (existingIndex !== -1)
+        uniquePeople[existingIndex].department.push(person.department);
+      else uniquePeople.push(obj);
+    });
+
+    return uniquePeople;
+  };
+
+  readyPeople();
+
   return (
     <div className={classes.peopleContainer}>
       <h2>{title}</h2>
-      {people.map((person) => {
+      {uniquePeople.map((person) => {
         const src = person.profile_path
           ? `https://image.tmdb.org/t/p/original/${person.profile_path}`
           : noImage;
@@ -27,13 +51,24 @@ const PeopleSection = ({ people, title }) => {
           >
             <img src={src} alt="Person profile img" />
             <p className={classes["person-name"]}>{person.name}</p>
-            <p className={classes["person-known_for"]}>
-              {person.character || person.department}
-            </p>
+            <div className={classes["person-known_for"]}>
+              {person.roles === undefined &&
+                person.department.map((item, index) => (
+                  <p key={index}>{item}</p>
+                ))}
+              {person.roles !== undefined &&
+                person.roles.map((item, index) => (
+                  <p key={index}>{item.character}</p>
+                ))}
+            </div>
           </div>
         );
       })}
-      { people.length === 0 && <p style={{margin: '20px auto'}}>Sorry, we couldn't found more data.</p> } 
+      {people.length === 0 && (
+        <p style={{ margin: "20px auto" }}>
+          Sorry, we couldn't found more data.
+        </p>
+      )}
     </div>
   );
 };
