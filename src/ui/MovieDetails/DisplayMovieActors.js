857772f66590fdd8cb9c13fbaa1ba981326c2eaf
Kamil Sajdera <kamilsajdera@gmail.com>
2024-03-06 16:54:02 +0100

    Added missing 'roles' in tv cast

--- src/ui/MovieDetails/DisplayMovieActors.js
+++ src/ui/MovieDetails/DisplayMovieActors.js
@@ -42,7 +42,7 @@ const DisplayMovieActors = ({ cast, id, title }) => {
                 <img src={actorProfileImg} alt="Profile img" />
                 <h3>{item.name}</h3>
                 <p>
-                  <i>{item.character}</i>
+                  <i>{item.character || item.roles.map(item => item.character)}</i>
                 </p>
               </div>
             );
