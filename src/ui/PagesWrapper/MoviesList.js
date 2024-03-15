2b7e63977d1be1001874df7360778bff3e07e0bc
Kamil Sajdera <kamilsajdera@gmail.com>
2024-03-15 17:16:35 +0100

    Added new hover effect to movie's image

--- src/ui/PagesWrapper/MoviesList.js
+++ src/ui/PagesWrapper/MoviesList.js
@@ -1,5 +1,8 @@
 import classes from "./MoviesList.module.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faUser } from "@fortawesome/free-solid-svg-icons";
+
 const MoviesList = (props) => {
   const { title, movies } = props;
 
@@ -17,6 +20,15 @@ const MoviesList = (props) => {
           key={movie.id}
           onClick={() => handleItemClick(index)}
         >
+          <div className={classes.overlay}>
+            <div className={classes["movie-date"]}>
+              {new Date(movie.release_date).toLocaleDateString("fr-CH")}
+            </div>
+            <div className={classes["movie-votes"]}>
+              <FontAwesomeIcon icon={faStar} /> {movie.vote_average.toFixed(2)}{" "}
+              / {movie.vote_count} <FontAwesomeIcon icon={faUser} />
+            </div>
+          </div>
           <img
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
