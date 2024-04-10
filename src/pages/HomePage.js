0f5f9f5b10a7da91b77f1de5ab624621d114f2dc
Kamil Sajdera <kamilsajdera@gmail.com>
2024-04-10 22:45:59 +0200

    Added welcome section in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,28 @@
+import classes from "./HomePage.module.css";
+
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <section className={classes.greetingSection}>
+      <h1>
+        <span>M</span>
+        <span>o</span>
+        <span>v</span>
+        <span>i</span>
+        <span>e</span>
+        <span>s</span>
+        <span> W</span>
+        <span>o</span>
+        <span>r</span>
+        <span>l</span>
+        <span>d</span>
+      </h1>
+      <p>
+        Welcome to a big library many of movies, tv series and people associated
+        with cinema. Discover movies thanks to our website with many categories
+        and details. Enjoy!
+      </p>
+    </section>
+  );
 };
 
 export default HomePage;
