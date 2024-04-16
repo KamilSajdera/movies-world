6d60897de5d3a444710edca47e5d058685bb9ca2
Kamil Sajdera <kamilsajdera@gmail.com>
2024-04-16 13:09:30 +0200

    Added swiper slider from 'swiper' library in TrendingSection

--- src/components/Home/TrendingSection.js
+++ src/components/Home/TrendingSection.js
@@ -1,8 +1,47 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import classes from "./TrendingSection.module.css";
+import "./custom-swiper.css";
 
 const TrendingSection = ({ items }) => {
-    console.log(items)
-  return <div className={classes.trendingSection}></div>;
+  return (
+    <div className={classes.trendingSection}>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        loop={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        navigation={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Navigation, Pagination]}
+        className={classes.mySwiper}
+      >
+        {items.map((item) => (
+          <SwiperSlide>
+            <img
+              src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+              alt={item.title}
+            />
+            <h3>{item.title}</h3>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default TrendingSection;
