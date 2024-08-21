569989f2927b39a01cbd7858c5e6e9d8f1804366
Kamil Sajdera <kamilsajdera@gmail.com>
2024-08-21 22:51:17 +0200

    Fetching filtered movies (using user filters) from API and passing to component

--- src/pages/DiscoverPage.js
+++ src/pages/DiscoverPage.js
@@ -1,9 +1,10 @@
-import { json, useLoaderData } from "react-router-dom";
+import { json, useActionData, useLoaderData } from "react-router-dom";
 import FiltersContainer from "../components/DiscoverPage/FiltersContainer";
 import ResultsContainer from "../components/DiscoverPage/ResultsContainer";
 
 const DiscoverPage = () => {
   const languagesFetchData = useLoaderData();
+  const filteredResults = useActionData();
   let languagesArray = [];
 
   for (const index in languagesFetchData) {
@@ -19,7 +20,7 @@ const DiscoverPage = () => {
   return (
     <>
       <FiltersContainer langs={languagesArray} />
-      <ResultsContainer />
+      <ResultsContainer results={filteredResults} />
     </>
   );
 };
@@ -61,11 +62,44 @@ export async function action({ request }) {
     release_start: formData.get("release_from"),
     release_end: formData.get("release_to"),
     language: formData.get("language").trim(),
-    sort: formData.get("sort_option"),
+    sort: formData.get("sort_option") || "popularity.desc",
     min_rated: formData.get("min_rated"),
     min_user_votes: formData.get("min_user_votes"),
-    with_people: formData.get("with_people").trim(),
+    with_keywords: formData.get("with_keywords").split(/\s+/).join(""),
   };
 
-  return {};
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.REACT_APP_BEARER_KEY}`,
+    },
+  };
+
+  let url = `https://api.themoviedb.org/3/discover/movie?&language=en-US&page=1&sort_by=${filterData.sort}`;
+
+  if (filterData.genres.length > 0)
+    url += `&with_genres=${filterData.genres.toString()}`;
+
+  if (filterData.release_start.length > 0)
+    url += `&primary_release_date.gte=${filterData.release_start}`;
+
+  if (filterData.release_end.length > 0)
+    url += `&primary_release_date.lte=${filterData.release_end}`;
+
+  if (filterData.language.length > 0)
+    url += `&with_original_language=${filterData.language}`;
+
+  if (filterData.min_rated.trim().length > 0)
+    url += `&vote_average.gte=${filterData.min_rated}`;
+
+  if (filterData.min_user_votes.trim().length > 0)
+    url += `&vote_count.gte=${filterData.min_user_votes}`;
+
+  if (filterData.with_keywords.trim().length > 0)
+    url += `&with_keywords=${filterData.with_keywords}`;
+
+  const response = await fetch(url, options);
+
+  return response;
 }
