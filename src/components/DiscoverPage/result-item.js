40ec37c2a2d018191890a57c19ab2455626a8870
Kamil Sajdera <kamilsajdera@gmail.com>
2024-09-19 19:41:31 +0200

    Added redirecting to clicked movie

--- src/components/DiscoverPage/result-item.js
+++ src/components/DiscoverPage/result-item.js
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 import classes from "./result-item.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-export default function ResultItem({ title, logo, description, rating }) {
+export default function ResultItem({ title, logo, description, rating, id }) {
+  const navigate = useNavigate();
+  function handleNavigate() {
+    navigate(
+      `/movie?id=${id}-${title.toLowerCase().replace(/\s+/g, "-").trim()}`
+    );
+  }
+
   return (
-    <div className={classes["result_item"]}>
+    <div className={classes["result_item"]} onClick={handleNavigate}>
       <img src={`https://image.tmdb.org/t/p/w500${logo}`} alt={title} />
       <div className={classes["result_data"]}>
         <h4>{title}</h4>
@@ -13,9 +22,7 @@ export default function ResultItem({ title, logo, description, rating }) {
           <FontAwesomeIcon icon={faStar} />
           <p>{rating}</p>
         </div>
-        <p className={classes.description}>
-            {description}
-        </p>
+        <p className={classes.description}>{description}</p>
       </div>
     </div>
   );
